2038145d1883c69c1e6e57c86484f5a9a49b5381
qiudaxian777 <137129580@qq.com>
2019-01-03 16:55:56 +0800

    增加一个MyHelper,代码

--- app/src/main/java/com/example/yt/communityinformationsystem/MainActivity.java
+++ app/src/main/java/com/example/yt/communityinformationsystem/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.yt.communityinformationsystem;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    MyHelper myHelper;
+    private EditText edtTxtName;
+    private EditText edtTxtId;
+    private TextView tvShow;
+    private Button btnAdd;
+    private Button btnQuery;
+    private Button btnUpdate;
+    private Button btnDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myHelper=new MyHelper(this);
+        init();
+    }
+    private void init() {
+        edtTxtName=(EditText) findViewById(R.id.edtTxt_main_name);
+        edtTxtId=(EditText) findViewById(R.id.edtTxt_main_id);
+        tvShow=(TextView)findViewById(R.id.tv_main_show);
+        btnAdd=(Button)findViewById(R.id.btn_main_add);
+        btnQuery=(Button)findViewById(R.id.btn_main_query);
+        btnUpdate=(Button)findViewById(R.id.btn_main_update);
+        btnDelete=(Button)findViewById(R.id.btn_main_delete);
+        btnAdd.setOnClickListener(this);
+        btnQuery.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String name;
+        String id;
+        SQLiteDatabase db;
+        ContentValues values;
+        switch (view.getId()){
+            case R.id.btn_main_add:
+                name=edtTxtName.getText().toString();
+                id=edtTxtId.getText().toString();
+                db=myHelper.getWritableDatabase();
+                values=new ContentValues();
+                values.put("name",name);
+                values.put("id",id);
+                db.insert("information",null,values);
+                Toast.makeText(this,"社团信息已添加",Toast.LENGTH_LONG).show();
+                db.close();;
+                break;
+            case R.id.btn_main_query:
+                db = myHelper.getReadableDatabase();
+                Cursor cursor=db.query("information",null,null,null,null,null,null);
+                if (cursor.getCount()==0){
+                    tvShow.setText("");
+                    Toast.makeText(this,"没有此社团信息",Toast.LENGTH_LONG).show();
+                } else {
+                    cursor.moveToFirst();
+                    tvShow.setText("Name :"+cursor.getString(1)+"; ID :"+cursor.getString(2));
+                }
+                while (cursor.moveToNext()){
+                    tvShow.append("\n"+"Name :"+cursor.getString(1)+" ; ID :"+cursor.getString(2));
+                }
+                cursor.close();
+                db.close();
+                break;
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/yt/communityinformationsystem/MyHelper.java
@@ -0,0 +1,21 @@
+package com.example.yt.communityinformationsystem;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyHelper extends SQLiteOpenHelper {
+    public MyHelper(Context context) {
+        super(context, "itcast.db",null ,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE information(_id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(20),id VARCHAR(20))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
