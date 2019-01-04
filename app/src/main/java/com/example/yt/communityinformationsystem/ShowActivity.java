065b8104d827d383418a417707132170aa829cc1
yylqrm <2855519475@qq.com>
2019-01-04 16:57:53 +0800

    更新了ShowActivity界面和新增代码

--- app/src/main/java/com/example/yt/communityinformationsystem/ShowActivity.java
+++ app/src/main/java/com/example/yt/communityinformationsystem/ShowActivity.java
@@ -3,11 +3,14 @@ package com.example.yt.communityinformationsystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvPassword;
+    private Button btnEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,16 @@ public class ShowActivity extends AppCompatActivity {
         tvPassword=(TextView)findViewById(R.id.tv_show_password);
         tvName.setText("用户名："+ name);
         tvPassword.setText("密     码："+ password);
+        btnEnter=(Button)findViewById(R.id.btn_show_enter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passDate();
+            }
+        });
+    }
+    public void passDate(){
+        Intent  intent1=new Intent(this,MainActivity.class);
+        startActivity(intent1);
     }
 }
