e77c9a0a72eed82c241701a143db6d1f353b3c47
yylqrm <2855519475@qq.com>
2019-01-04 16:41:13 +0800

    转换了开始的Activity，新增Register和Show 的代码

--- app/src/main/java/com/example/yt/communityinformationsystem/RegisterActivity.java
+++ app/src/main/java/com/example/yt/communityinformationsystem/RegisterActivity.java
@@ -1,13 +1,35 @@
 package com.example.yt.communityinformationsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText edtTxtName;
+    private EditText edtTxtPassword;
+    private Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edtTxtName=(EditText)findViewById(R.id.edtTxt_regist_name);
+        edtTxtPassword=(EditText)findViewById(R.id.edtTxt_regist_password);
+        btnSend=findViewById(R.id.btn_regist_send);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passDate();
+            }
+        });
+    }
+    public void passDate(){
+        Intent intent=new Intent(this,ShowActivity.class);
+        intent.putExtra("name",edtTxtName.getText().toString().trim());
+        intent.putExtra("password",edtTxtPassword.getText().toString().trim());
+        startActivity(intent);
     }
 }
